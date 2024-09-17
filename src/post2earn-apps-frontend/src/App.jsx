d5307ff9a7e643a44a02a61d6f0e29dfe4b22aaa
fajarramadhann <ramadhan28.fajar@gmail.com>
2024-09-17 08:06:30 +0700

    Initial commit.

---
+++ src/post2earn-apps-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { post2earn_apps_backend } from 'declarations/post2earn-apps-backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    post2earn_apps_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
